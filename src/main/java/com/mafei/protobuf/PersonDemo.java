6fcaa0e6e90bd2b46f2e74ba8e97221d6f37a1dc
mafei <mafei.dev@gmail.com>
2021-06-24 12:54:02 +0530

    serialize and deserialize the person object and check the equality.

--- src/main/java/com/mafei/protobuf/PersonDemo.java
+++ src/main/java/com/mafei/protobuf/PersonDemo.java
@@ -2,13 +2,30 @@ package com.mafei.protobuf;
 
 import com.mafei.model.Person;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 /**
  * @Author mafei
  * @Created 6/24/2021 12:29 PM
  */
 public class PersonDemo {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         Person mafei = Person.newBuilder().setName("mafei").setAge(25).build();
         System.out.println("mafei = " + mafei);
+
+        //serialize the object
+        Path path = Paths.get("mafei.person.ser");
+        Files.write(path, mafei.toByteArray());
+
+        //deserialize the object
+        byte[] mafieBytes = Files.readAllBytes(path);
+        Person mafeiFormFile = Person.parseFrom(mafieBytes);
+        System.out.println("mafeiFormFile = " + mafeiFormFile);
+
+        System.out.println("equality = " + (mafei == mafeiFormFile));
+        System.out.println("equality by data = " + (mafei.equals(mafeiFormFile)));
     }
 }
