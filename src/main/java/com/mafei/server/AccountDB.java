2445afffde49b88e20e58ce56f4d861905412d61
mafei <mafei.dev@gmail.com>
2021-06-25 04:06:51 +0530

    adding database in memory

---
+++ src/main/java/com/mafei/server/AccountDB.java
@@ -0,0 +1,41 @@
+package com.mafei.server;
+
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+/**
+ * @Author mafei
+ * @Created 6/25/2021 3:54 AM
+ */
+public class AccountDB {
+
+
+    private static final Map<Integer, Integer> database =
+            IntStream
+                    .rangeClosed(1, 10)
+                    .boxed()
+                    .collect(
+                            Collectors.toMap(Function.identity(), integer -> {
+                                return integer * 10;
+                            })
+                    );
+
+
+    public static Integer addAmount(int accountNumber, int amount) {
+        return database.computeIfPresent(accountNumber, (_accountNum, _amount) -> {
+            return _amount + amount;
+        });
+    }
+
+    public static Integer deductAmount(int accountNumber, int amount) {
+        return database.computeIfPresent(accountNumber, (_accountNum, _amount) -> {
+            return _amount - amount;
+        });
+    }
+
+    public static Integer getBalance(int accountNumber) {
+        return database.get(accountNumber);
+    }
+}
